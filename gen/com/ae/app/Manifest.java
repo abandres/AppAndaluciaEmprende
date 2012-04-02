0d5e988b7b37c531b50c2dbd2888535f4287eb5c
Alberto Bandres Pereira <bandrespereira.alberto@gmail.com>
2012-04-02 13:36:03 +0200

    v0002

--- gen/com/ae/app/Manifest.java
+++ gen/com/ae/app/Manifest.java
@@ -9,7 +9,21 @@ package com.ae.app;
 
 public final class Manifest {
     public static final class permission {
-        /**  REQUIRED for C2DM 
+        /** 
+    <uses-permission android:name="android.permission.VIBRATE" />
+    <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
+    <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+    <uses-permission android:name="android.permission.ACCESS_LOCATION_EXTRA_COMMANDS" />
+    <uses-permission android:name="android.permission.READ_PHONE_STATE" />
+    <uses-permission android:name="android.permission.INTERNET" />
+    <uses-permission android:name="android.permission.READ_CONTACTS" />
+    <uses-permission android:name="android.permission.WRITE_CONTACTS" />
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+    <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+    <uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED" />
+    <uses-permission android:name="android.permission.READ_LOGS" />
+
+ REQUIRED for C2DM 
  Only this application can receive the messages and registration result 
          */
         public static final String C2D_MESSAGE="com.ae.app.permission.C2D_MESSAGE";
